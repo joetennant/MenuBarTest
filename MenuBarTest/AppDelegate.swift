bdcc2d020a2e189dbf5e8e25063a7d7104cd1582
Joe Tennant <joetennant@gmail.com>
2019-01-11 14:36:34 -0500

    Initial Commit

---
+++ MenuBarTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MenuBarTest
+//
+//  Created by Joseph Tennant on 1/11/19.
+//  Copyright © 2019 7SIGNAL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
